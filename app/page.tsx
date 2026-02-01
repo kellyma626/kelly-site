32869f4f63146e201b6b68dc4a6b45d5e5e6411f
kelly <kelly@Kellys-MacBook-Air-4.local>
2026-01-31 22:04:34 -0800

    added starter code

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Hero from "@/components/Hero";
+
+const Page = () => {
+  return (
+    <div>
+      <Hero />
+      
+    </div>
+  );
+}
+
+export default Page;
---
+++ components/Hero.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+const Hero = () => {
+  return (
+    <div>
+        <div className="text-kelly-pink-500">Welcome to my personal portfolio website!</div>
+        <div className="">Welcome to my personal portfolio website!</div>
+        <div className="text-[var(--kelly-pink-500)]"> hi </div>
+
+    </div>
+  );
+}
+
+export default Hero;
